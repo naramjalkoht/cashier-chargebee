3bff321df4c6a90a916c3951a9ac5a91a493da54
Anna Karykowska <anna@threadable.io>
2025-01-09 14:29:27 +0100

    feat(provider): create CashierServiceProvider with migration publishing

---
+++ src/CashierServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Laravel\CashierChargebee;
+
+use Illuminate\Support\ServiceProvider;
+
+class CashierServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPublishing();
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Register the package's publishable resources.
+     *
+     * @return void
+     */
+    protected function registerPublishing()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../database/migrations' => $this->app->databasePath('migrations'),
+            ], 'cashier-migrations');
+        }
+    }
+}
