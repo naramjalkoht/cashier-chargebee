38933b1c3757a70db24dcbde0bbebc5d19bae77d
Anna Karykowska <anna@threadable.io>
2025-01-13 10:26:20 +0100

    feat(provider): in CashierServiceProvider add merging ccashier.php file to configs and publishing that file

---
+++ src/CashierServiceProvider.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Laravel\CashierChargebee;
+
+use Illuminate\Support\ServiceProvider;
+
+class CashierServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPublishing();
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->configure();
+    }
+
+    /**
+     * Setup the configuration for Cashier.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/cashier.php', 'cashier'
+        );
+    }
+
+    /**
+     * Register the package's publishable resources.
+     *
+     * @return void
+     */
+    protected function registerPublishing()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/cashier.php' => $this->app->configPath('cashier.php'),
+            ], 'cashier-config');
+        }
+    }
+}
