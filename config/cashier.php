eb7771c18ee7995748e367f2e3e329e559e36053
Anna Karykowska <anna@threadable.io>
2025-01-16 15:44:57 +0100

    feat(config): add views path, currency, and currency_locale settings

--- config/cashier.php
+++ config/cashier.php
@@ -16,4 +16,43 @@ return [
     'site' => env('CHARGEBEE_SITE'),
 
     'api_key' => env('CHARGEBEE_API_KEY'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cashier Path
+    |--------------------------------------------------------------------------
+    |
+    | This is the base URI path where Cashier's views, such as the payment
+    | verification screen, will be available from. You're free to tweak
+    | this path according to your preferences and application design.
+    |
+    */
+
+    'path' => env('CASHIER_PATH', 'chargebee'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Currency
+    |--------------------------------------------------------------------------
+    |
+    | This is the default currency that will be used when generating charges
+    | from your application. Of course, you are welcome to use any of the
+    | various world currencies that are currently supported via Chargebee.
+    |
+    */
+
+    'currency' => env('CASHIER_CURRENCY', 'usd'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Currency Locale
+    |--------------------------------------------------------------------------
+    |
+    | This is the default locale in which your money values are formatted in
+    | for display. To utilize other locales besides the default en locale
+    | verify you have the "intl" PHP extension installed on the system.
+    |
+    */
+
+    'currency_locale' => env('CASHIER_CURRENCY_LOCALE', 'en'),
 ];
