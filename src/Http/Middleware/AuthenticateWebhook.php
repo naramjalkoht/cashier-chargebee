8f37bb45525ecf325517504c6f0de6d583cec6e8
Anna Karykowska <anna@threadable.io>
2025-01-17 14:24:34 +0100

    feat(webhook): add AuthenticateWebhook middleware for securing webhook route using Basic Authentication

---
+++ src/Http/Middleware/AuthenticateWebhook.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Laravel\CashierChargebee\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthenticateWebhook
+{
+    /**
+     * Handle an incoming request.
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $username = config('cashier.webhook.username');
+        $password = config('cashier.webhook.password');
+
+        if (!$request->hasHeader('Authorization')) {
+            return new Response('Unauthorized', 401);
+        }
+
+        $authorization = $request->header('Authorization');
+        if (str_starts_with($authorization, 'Basic ')) {
+            $credentials = base64_decode(substr($authorization, 6));
+            [$requestUsername, $requestPassword] = explode(':', $credentials, 2);
+
+            if ($requestUsername === $username && $requestPassword === $password) {
+                return $next($request);
+            }
+        }
+        
+        return new Response('Unauthorized', 401);
+    }
+}
