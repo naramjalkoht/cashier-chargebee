3ff4002576e4d482f041041ea007ce9417d78346
Anna Karykowska <anna@threadable.io>
2025-01-17 14:23:38 +0100

    feat(webhooks): add WebhookController and WebhookReceived event to handle incoming webhook requests

---
+++ src/Events/WebhookReceived.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Laravel\CashierChargebee\Events;
+
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class WebhookReceived
+{
+    use Dispatchable, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(
+        public array $payload
+    ) {}
+}
