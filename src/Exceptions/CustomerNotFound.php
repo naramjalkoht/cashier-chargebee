9c19aed10ca3947a32a75ebf0510479c925bceb0
Anna Karykowska <anna@threadable.io>
2025-01-20 18:14:02 +0100

    feat(customers): add asChargebeeCustomer() method for retrieving customers with chargebee_id

---
+++ src/Exceptions/CustomerNotFound.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Laravel\CashierChargebee\Exceptions;
+
+use Exception;
+use Illuminate\Database\Eloquent\Model;
+
+final class CustomerNotFound extends Exception
+{
+    /**
+     * Create a new InvalidCustomer instance.
+     */
+    public static function notFound(Model $owner): static
+    {
+        return new static(class_basename($owner).' is not a Chargebee customer yet. See the createAsChargebeeCustomer method.');
+    }
+}
