a428bc9c0cac29f258d862c36eb6319dbcc3eaaa
Padraig Nolan <padraig@threadable.io>
2025-01-23 14:01:50 +0000

    [CI] C4CB-40 DONE

--- src/Subscription.php
+++ src/Subscription.php
@@ -3,7 +3,9 @@
 namespace Laravel\CashierChargebee;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\CashierChargebee\Concerns\Prorates;
 
 class Subscription extends Model
 {
+    use Prorates;
 }
