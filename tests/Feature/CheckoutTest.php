546628ab4cf9d62ad5faa62a02e77a722f41aace
Padraig Nolan <padraig@threadable.io>
2025-01-23 14:31:13 +0000

    [CI] C4CB-41 DONE

---
+++ tests/Feature/CheckoutTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Laravel\CashierChargebee\Tests\Feature;
+
+use Laravel\CashierChargebee\Subscription;
+
+
+class CheckoutTest extends FeatureTestCase
+{
+
+    public function test_customers_can_start_a_product_checkout_session()
+    {
+        $subscription = (new Subscription());
+        $this->assertNotNull($subscription->checkoutDiscounts());
+    }
+
+    public function test_customers_can_start_a_product_checkout_session_with_a_coupon_applied()
+    {
+        $subscription = (new Subscription())->withCoupons(['TEST-123']);
+        $this->assertNotNull($subscription->checkoutDiscounts());
+    }
+
+}
