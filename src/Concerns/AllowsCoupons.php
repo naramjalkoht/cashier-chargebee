dea7972ff8915d8a819b0d0c49a474048bd06aef
Padraig Nolan <padraig@threadable.io>
2025-01-23 14:22:51 +0000

    [WIP]

---
+++ src/Concerns/AllowsCoupons.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Laravel\CashierChargebee\Concerns;
+
+trait AllowsCoupons
+{
+    /**
+     * The coupon IDs being applied.
+     *
+     * @var array
+     */
+    public $couponIds = [];
+
+    /**
+     * The coupon IDs to be applied.
+     *
+     * @param  array  $couponIds
+     * @return $this
+     */
+    public function withCoupons($couponIds): self
+    {
+        $this->couponIds = $couponIds;
+
+        return $this;
+    }
+
+    /**
+     * Return the discounts for a Chargebee Checkout session.
+     *
+     * @return array[]|null
+     */
+    protected function checkoutDiscounts()
+    {
+        if (count($this->couponIds)) {
+            return [['coupon_ids' => $this->couponIds]];
+        }
+    }
+}
--- src/Subscription.php
+++ src/Subscription.php
@@ -3,9 +3,12 @@
 namespace Laravel\CashierChargebee;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\CashierChargebee\Concerns\AllowsCoupons;
 use Laravel\CashierChargebee\Concerns\Prorates;
 
 class Subscription extends Model
 {
     use Prorates;
+    use AllowsCoupons;
+
 }
